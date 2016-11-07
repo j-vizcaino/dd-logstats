7f82a1232ccd1fd7ef9a692b3f9d47c93318293a
Jérôme Vizcaino <jerome.vizcaino@gmail.com>
2016-11-08 00:22:30 +0100

    Add tracker class for stats per section

---
+++ stats.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// SectionStats holds statistics for a given URL section
+type SectionStats struct {
+	SectionName    string
+	HitCount       int64
+	ReturnedCode   map[string]int
+	BytesPerClient map[string]int64
+}
+
+// Helper function: transform HTTP return code into code family (1xx, 2xx, 3xx, etc...)
+func codeFamily(returnCode int) string {
+	c := returnCode / 100
+	return fmt.Sprintf("%dxx", c)
+}
+
+func newSectionStats(name string) *SectionStats {
+	return &SectionStats{
+		SectionName:    name,
+		ReturnedCode:   make(map[string]int),
+		BytesPerClient: make(map[string]int64),
+	}
+}
+
+// AddLogEntry increases the current SectionStats statistics
+// with the give LogEntry
+func (s *SectionStats) AddLogEntry(l *LogEntry) {
+	s.HitCount++
+	k := codeFamily(l.HTTP.ReturnCode)
+	s.ReturnedCode[k]++
+	s.BytesPerClient[l.ClientIP] += l.SizeBytes
+}
+
+// Stats holds statistics grouped by URL section
+type Stats struct {
+	Sections []*SectionStats
+	ByName   map[string]*SectionStats
+}
+
+// NewStats creates a new empty Stats object
+func NewStats() *Stats {
+	return &Stats{
+		ByName: make(map[string]*SectionStats),
+	}
+}
+
+// AddLogEntry adds a new LogEntry to the stats
+func (h *Stats) AddLogEntry(l *LogEntry) {
+	section := l.URLSection()
+
+	s, ok := h.ByName[section]
+	if !ok {
+		s = newSectionStats(section)
+		h.Sections = append(h.Sections, s)
+		h.ByName[section] = s
+	}
+	s.AddLogEntry(l)
+}
+
+// SectionsByHits returns a slice of SectionStats sorted by hit count.
+// The section with the most hits comes first, the one with the least, last
+func (h *Stats) SectionsByHits() []*SectionStats {
+	moreHits := func(a, b *SectionStats) bool {
+		return a.HitCount > b.HitCount
+	}
+	stats := h.Sections
+	by(moreHits).Sort(stats)
+	return stats
+}
+
+//
+// Custom sort helpers
+//
+
+type by func(a, b *SectionStats) bool
+
+func (b by) Sort(s []*SectionStats) {
+	sorter := &sectionStatSorter{
+		stats:   s,
+		cmpFunc: b,
+	}
+	sort.Sort(sorter)
+}
+
+type sectionStatSorter struct {
+	stats   []*SectionStats
+	cmpFunc by
+}
+
+func (s *sectionStatSorter) Len() int {
+	return len(s.stats)
+}
+
+func (s *sectionStatSorter) Swap(i, j int) {
+	s.stats[i], s.stats[j] = s.stats[j], s.stats[i]
+}
+
+func (s *sectionStatSorter) Less(i, j int) bool {
+	return s.cmpFunc(s.stats[i], s.stats[j])
+}
---
+++ stats_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCodeFamily(t *testing.T) {
+	assert.Equal(t, "1xx", codeFamily(105))
+	assert.Equal(t, "4xx", codeFamily(404))
+	// does not care about return code range
+	assert.Equal(t, "9xx", codeFamily(999))
+}
+
+var fooEntries = []*LogEntry{
+	&LogEntry{
+		ClientIP:  "1.1.1.1",
+		URL:       "/foo/1",
+		HTTP:      HTTPInfo{ReturnCode: 200},
+		SizeBytes: 123,
+	},
+	&LogEntry{
+		ClientIP:  "1.1.1.1",
+		URL:       "/foo/2",
+		HTTP:      HTTPInfo{ReturnCode: 404},
+		SizeBytes: 321,
+	},
+	&LogEntry{
+		ClientIP:  "1.1.1.2",
+		URL:       "/foo/1",
+		HTTP:      HTTPInfo{ReturnCode: 201},
+		SizeBytes: 987,
+	},
+}
+
+func testFooSection(t *testing.T, s *SectionStats) {
+	expectedBytes := map[string]int64{
+		"1.1.1.1": int64(123 + 321),
+		"1.1.1.2": int64(987),
+	}
+	assert.Equal(t, "foo", s.SectionName)
+	assert.EqualValues(t, expectedBytes, s.BytesPerClient)
+	assert.Equal(t, map[string]int{"2xx": 2, "4xx": 1}, s.ReturnedCode)
+	assert.EqualValues(t, len(fooEntries), s.HitCount)
+}
+
+var barEntries = []*LogEntry{
+	&LogEntry{
+		ClientIP:  "1.1.2.1",
+		URL:       "/bar/1",
+		HTTP:      HTTPInfo{ReturnCode: 301},
+		SizeBytes: 12,
+	},
+	&LogEntry{
+		ClientIP:  "1.1.2.1",
+		URL:       "/bar/2",
+		HTTP:      HTTPInfo{ReturnCode: 404},
+		SizeBytes: 4096,
+	},
+}
+
+func testBarSection(t *testing.T, s *SectionStats) {
+	expectedBytes := map[string]int64{
+		"1.1.2.1": int64(12 + 4096),
+	}
+	assert.Equal(t, "bar", s.SectionName)
+	assert.EqualValues(t, expectedBytes, s.BytesPerClient)
+	assert.Equal(t, map[string]int{"3xx": 1, "4xx": 1}, s.ReturnedCode)
+	assert.EqualValues(t, len(barEntries), s.HitCount)
+}
+
+func TestSectionStatsAddLogEntry(t *testing.T) {
+	s := newSectionStats("foo")
+
+	for _, f := range fooEntries {
+		s.AddLogEntry(f)
+	}
+	testFooSection(t, s)
+}
+
+func TestStatsAddLogEntry(t *testing.T) {
+	s := NewStats()
+
+	for _, l := range barEntries {
+		s.AddLogEntry(l)
+	}
+	assert.Len(t, s.Sections, 1)
+	assert.Len(t, s.ByName, 1)
+	assert.Equal(t, s.Sections[0], s.ByName["bar"])
+	testBarSection(t, s.ByName["bar"])
+
+	for _, l := range fooEntries {
+		s.AddLogEntry(l)
+	}
+	assert.Len(t, s.Sections, 2)
+	assert.Len(t, s.ByName, 2)
+	assert.NotNil(t, s.ByName["foo"])
+	testFooSection(t, s.ByName["foo"])
+
+	hits := s.SectionsByHits()
+	assert.Len(t, hits, 2)
+	// Sort by hits, DESC: expect [foo, bar]
+	assert.Equal(t, hits[0], s.ByName["foo"])
+	assert.Equal(t, hits[1], s.ByName["bar"])
+}
